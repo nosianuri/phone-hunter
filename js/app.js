7ff53bea0cd24f3c9cdb72bf240c27c78f19db15
nosianuri <nurinosia@gmail.com>
2022-03-01 20:05:14 +0600

    phone search

--- js/app.js
+++ js/app.js
@@ -1,11 +1,34 @@
 const allPhone = () => {
     const searchValue = document.getElementById("search-box").value;
 
-const url =  `https://openapi.programming-hero.com/api/phones?search=${searchValue}`
-console.log(url);
-fetch(url)
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+    const url =  `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
+//  console.log(url);
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => showPhoneDetails(data.data));
 
-    console.log(searchValue);
+    // console.log(searchValue);
 };
+
+const showPhoneDetails = (phones) => {
+   for (const phone of phones) {
+    const parent=document.getElementById("phone-container");
+
+    const div = document.createElement("div");
+    div.classList.add('col');
+    div.innerHTML = `
+    <div class="card h-100">
+        <img class:"w-25" src="${phone.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h2 class="card-title">Name: ${phone.phone_name}</h2>
+        <h5>Brand: ${phone.brand}</h5>
+            <div class="allbutton">
+                <button class="btn btn-success">Delete</button>
+            </div>	
+        </div>
+  </div>`
+parent.appendChild(div);
+console.log(phone);
+   } 
+    
+}
