3dc252f18cc52b3f6720fa2283ff42b0a25d802d
nosianuri <nurinosia@gmail.com>
2022-03-02 01:24:53 +0600

    details customize

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 const allPhone = () => {
-    document.getElementById("phone-container").innerHTML = "";
-    const searchValue = document.getElementById("search-box").value;
     // clear data
-    
+    document.getElementById("phone-container").innerHTML = "";
 
+    const searchValue = document.getElementById("search-box").value;
+    
     // load data
     const url =  `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
+
 //  console.log(url);
     fetch(url)
         .then((response) => response.json())
@@ -17,6 +18,12 @@ const allPhone = () => {
 const showPhoneDetails = (phones) => {
    for (const phone of phones) {
     const parent=document.getElementById("phone-container");
+    if(phones.length == 0 || phones.data
+        == null) {
+            document.getElementById("null-phone").style.display = 'block';
+        } else {
+            ("null-phone").style.display = 'none';
+        }
 
     const div = document.createElement("div");
     div.classList.add('col');
@@ -53,24 +60,24 @@ const displayPhoneDetail = (phone) => {
     <div class="text-center p-3">
     <img src="${phone.image}" class="card-img-top" alt="...">
             <div class="card-body">
-              <h5>${phone.mainFeatures.phone_name}</h5>
-              <h5>${phone.mainFeatures.sensors}</h5>
-              <h5>${phone.brand}</h5>
-              <h5>${phone.mainFeatures.storage}</h5>
-              <h5>${phone.mainFeatures.displaySize}</h5>
-              <h5>${phone.mainFeatures.chipSet}</h5>
-              <h5>${phone.mainFeatures.memory}</h5>
-              <h5>${phone.mainFeatures.sensors}</h5>
-              <h5>${phone.slug}</h5>
-              <h5>${phone.releaseDate}</h5>
-              <h5>${phone.Bluetooth}</h5>
-              <h5>${phone.GPS}</h5>
-              <h5>${phone.NFC}</h5>
-              <h5>${phone.mainFeatures[1]}</h5>
-              <h5>${phone.mainFeatures.USB}</h5>
-              
+              <h5>name: ${phone.name}</h5>
+              <h5>sensors: ${phone.mainFeatures.sensors}</h5>
+              <h5>brand: ${phone.brand}</h5>
+              <h5>storage: ${phone.mainFeatures.storage}</h5>
+              <h5>displaySize: ${phone.mainFeatures.displaySize}</h5>
+              <h5>chipSet: ${phone.mainFeatures.chipSet}</h5>
+              <h5>memory: ${phone.mainFeatures.memory}</h5>
+              <h5>sensors: ${phone.mainFeatures.sensors}</h5>
+              <h5>slug: ${phone.slug}</h5>
+              <h5>releaseDate: ${phone.releaseDate}</h5>
+              <h5>Bluetooth: ${phone.others.Bluetooth}</h5>
+              <h5>GPS: ${phone.others.GPS}</h5>
+              <h5>NFC: ${phone.others.NFC}</h5>
+              <h5>USB: ${phone.others.USB}</h5>
+              <h5>wlan: ${phone.others.WLAN}</h5>
             </div>
             </div>
     `;
     phoneDetails.appendChild(div);
+    console.log(phone);
 }
