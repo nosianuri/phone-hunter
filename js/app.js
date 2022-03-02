9766cd9ee51e411810262e7e4e90918efa0f0547
nosianuri <nurinosia@gmail.com>
2022-03-02 18:48:17 +0600

    api link set

--- js/app.js
+++ js/app.js
@@ -1,28 +1,37 @@
 const allPhone = () => {
-    // clear data
+    /***********************clear data************************ */ 
     document.getElementById("phone-container").innerHTML = "";
+
     document.getElementById("spinner").style.display = "block";
-    const searchValue = document.getElementById("search-box").value;
+    const searchField = document.getElementById("search-box");
+    const searchValue = searchField.value;
+
+    // clear data
+    searchField.value = '';
     
-    // load data
+    /*******************************load data*********************************** */ 
     const url =  `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
 
 
-//  console.log(url);
+
     fetch(url)
         .then((response) => response.json())
         .then((data) => {
-            console.log(data.data==0)
-            if (data.data == 0) {
+            console.log(data.data.slice(0, 20) == 0)
+            
+            if (data.data.slice(0, 20) == 0) {
                 document.getElementById("null-phone").style.display = "block";
-            } else {
-                showPhoneDetails(data.data);
+            }  else{
+                showPhoneDetails(data.data.slice(0, 20));
+                
                 document.getElementById("null-phone").style.display = "none";
             }
         });
        document.getElementById("spinner").style.display = "none"; 
-    // console.log(searchValue);
+    
 };
+
+/*******************all phone details******************** */ 
 
 const showPhoneDetails = (phones) => {
    for (const phone of phones) {
@@ -33,8 +42,8 @@ const showPhoneDetails = (phones) => {
     
     div.classList.add('col');
     div.innerHTML = `
-    <div class="card h-100">
-        <img class:"w-25" src="${phone.image}" class="card-img-top" alt="...">
+    <div class="card h-100 col-md-12 m-auto">
+        <img src="${phone.image}" class="card-img-top w-25" alt="...">
         <div class="card-body">
         <h2 class="card-title">Name: ${phone.phone_name}</h2>
         <h5>Brand: ${phone.brand}</h5>
@@ -42,14 +51,14 @@ const showPhoneDetails = (phones) => {
                 <button onclick="loadPhoneDetail('${phone.slug}')" class="btn btn-success">Delete</button>
             </div>	
         </div>
-  </div>`;
+        </div>`;
 parent.appendChild(div);
 console.log(phone);
    } 
 }
-
+/***************************single phone details******************************* */  
 const loadPhoneDetail = (phoneId) => {
-    // console.log(phoneId);
+    
     const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
     fetch(url)
         .then(res => res.json())
@@ -57,8 +66,9 @@ const loadPhoneDetail = (phoneId) => {
 }
 
 const displayPhoneDetail = (phone) => {
-    // console.log(phone);
+    
      const phoneDetails = document.getElementById('phone-details');
+     phoneDetails.textContent = '';
     const div = document.createElement('div');
     window.scrollTo(0,0);
     div.classList.add('card');
@@ -66,22 +76,21 @@ const displayPhoneDetail = (phone) => {
     <div class="text-center p-3">
     <img src="${phone.image}" class="card-img-top" alt="...">
             <div class="card-body">
-              <h5>name: ${phone.name}</h5>
-              <h5>releaseDate: ${phone.releaseDate?phone.releaseDate:"No release date found"}</h5>
-              <h5>sensors: ${phone.mainFeatures.sensors}</h5>
-              <h5>brand: ${phone.brand}</h5>
-              <h5>storage: ${phone.mainFeatures.storage}</h5>
-              <h5>displaySize: ${phone.mainFeatures.displaySize}</h5>
-              <h5>chipSet: ${phone.mainFeatures.chipSet}</h5>
-              <h5>memory: ${phone.mainFeatures.memory}</h5>
-              <h5>sensors: ${phone.mainFeatures.sensors}</h5>
-              <h5>slug: ${phone.slug}</h5>
-              
-              <h5>Bluetooth: ${phone.others.Bluetooth}</h5>
-              <h5>GPS: ${phone.others.GPS}</h5>
-              <h5>NFC: ${phone.others.NFC}</h5>
-              <h5>USB: ${phone.others.USB}</h5>
-              <h5>wlan: ${phone.others.WLAN}</h5>
+              <h5>name:<small>  ${phone.name}<small></h5>
+              <h5>releaseDate:<small> ${phone.releaseDate?phone.releaseDate:"No release date found"}<small></h5>
+              <h5>sensors:<small> ${phone.mainFeatures.sensors}<small></h5>
+              <h5>brand:<small>  ${phone.brand}<small></h5>
+              <h5>storage:<small>  ${phone.mainFeatures.storage}<small></h5>
+              <h5>displaySize:<small>  ${phone.mainFeatures.displaySize}<small></h5>
+              <h5>chipSet: <small> ${phone.mainFeatures.chipSet}<small></h5>
+              <h5>memory: <small> ${phone.mainFeatures.memory}<small></h5>
+              <h5>sensors: <small> ${phone.mainFeatures.sensors}<small></h5>
+              <h5>slug: <small> ${phone.slug}<small></h5>
+              <h5>Bluetooth: <small> ${phone.others.Bluetooth}<small></h5>
+              <h5>GPS: <small> ${phone.others.GPS}<small></h5>
+              <h5>NFC: <small> ${phone.others.NFC}<small></h5>
+              <h5>USB: <small> ${phone.others.USB}<small></h5>
+              <h5>wlan: <small> ${phone.others.WLAN}<small></h5>
             </div>
             </div>
     `;
